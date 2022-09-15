dc232bff2c385558a95839d6cd23775ec3fec892
A.J. Miller <ajmiller1206@gmail.com>
2022-09-15 13:08:48 -0400

    first commit

---
+++ MethodsWithLoopsAndConditionals/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    public static class Class1
+    {
+        public static void Print1000()
+        {
+            for (int i = 1000; i > -1000; i--)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine(-1000);
+        }
+
+        public static void PrintThrees()
+        {
+            for (int i = 3; i < 999; i = i + 3)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine(999);
+        }
+
+        public static void AreEqual(int x, int y)
+        {
+            if (x == y) Console.WriteLine("The numbers are equal.");
+            else Console.WriteLine("The numbers are not equal.");
+        }
+
+        public static void IsEven(int x)
+        {
+            if (x % 2 == 0) Console.WriteLine("The number is even.");
+            else Console.WriteLine("The number is odd.");
+        }
+
+        public static void IsPositive(int x)
+        {
+            if (x == 0) Console.WriteLine("The number is neither negative nor positive.");
+            else if (x == Math.Abs(x)) Console.WriteLine("The number is positive.");
+            else Console.WriteLine("The number is negative.");
+        }
+
+        public static void CanVote(int x)
+        {
+            if (x >= 18)
+            {
+                Console.WriteLine("You are old enough to vote!");
+            }
+            else if (x <= 0)
+            {
+                Console.WriteLine("You have not yet been born.");
+            }
+            else
+            {
+                Console.WriteLine("You can't vote yet.");
+            }
+        }
+
+        public static void Range10(int x)
+        {
+            if (x <= 10 && x >= -10)
+            {
+                Console.WriteLine("Number is within range.");
+            }
+            else
+            {
+                Console.WriteLine("Number is out of range.");
+            }
+        }
+
+        public static void MultiplicationTable(int x)
+        {
+            for (int i = 1; i < 12; i++)
+            {
+                Console.Write($"{x * i}, ");
+            }
+            Console.WriteLine(x * 12);
+        }
+    }
+}
---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MethodsWithLoopsAndConditionals;
+using System.Security.Cryptography.X509Certificates;
+using static MethodsWithLoopsAndConditionals.Class1;
+
+Console.WriteLine("Type \"count backwards\" and the console will print out all of the numbers from 1000 to -1000.");
+bool cont = true;
+do
+{
+    if (Console.ReadLine().ToLower() == "count backwards")
+    {
+        cont = false;
+    }
+    else
+    {
+        cont = true;
+        Console.WriteLine("Program will not continue until you type \"count backwards\"");
+    }
+} while (cont == true);
+Print1000();
+
+Console.WriteLine("\nType \"count threes\" and the program will count by threes to 999.");
+cont = true;
+do
+{
+    if (Console.ReadLine().ToLower() == "count threes")
+    {
+        cont = false;
+    }
+    else
+    {
+        cont = true;
+        Console.WriteLine("Program will not continue until you type \"count threes\".");
+    }
+} while (cont == true);
+PrintThrees();
+
+/*Past this point, I wanted to prevent the program from breaking if you inputed the wrong value type,
+but it was too time consuming to figure out for now.
+*/
+
+Console.Write("\nInput two numbers to see if they are equal.\n First number: ");
+int isEqual1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second number: ");
+int isEqual2 = Convert.ToInt32(Console.ReadLine());
+AreEqual(isEqual1, isEqual2);
+
+Console.WriteLine("\n Input a number to see if it is even.");
+IsEven(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine("\nInput a number to see if it is positive or negative.");
+IsPositive(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine("\nPlease input your age");
+CanVote(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine("\nPlease input an integer to see if it's within the range -10 to 10.");
+Range10(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine("\nPlease input an integer to see its multiplication table.");
+MultiplicationTable(Convert.ToInt32(Console.ReadLine()));
